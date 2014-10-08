db6d42ce0c765ee385a1a989850e1ad5e7e93acb
Yuriy Syrovetskiy <cblp@cblp.su>
2014-10-08 18:41:02 +0400

    [+] started working on tags

--- build.hs
+++ build.hs
@@ -1,6 +1,10 @@
 #!/usr/bin/env runhaskell
 {-# OPTIONS -Wall -Werror #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
+import Control.Monad (forM)
+import Data.List (elemIndices)
+--import Data.Map (Map)
 import Development.Shake
 
 
@@ -8,7 +12,25 @@ main :: IO ()
 main = shakeArgs shakeOptions $ do
     want ["_site"]
 
-    "_site" ~> jekyll_build
+    "_site" ~> do
+        need ["tags"]
+        cmd "jekyll" "build"
 
-    where
-        jekyll_build = cmd "jekyll" "build"
+    "tags" ~> do
+        postFiles <- getDirectoryFiles "" ["_posts/*.md"]
+
+        -- read tags
+        tags :: [(Tag, FilePath)] <- do
+            forM postFiles $ \postFile -> do
+                postFileLines <- readFileLines postFile
+                let frontmatterStart:frontmatterEnd:_ =
+                        elemIndices "---" postFileLines
+                liftIO $ print (frontmatterStart, frontmatterEnd)
+                return ("tag", postFile)
+        liftIO $ print tags
+
+        -- TODO: write tag list
+        -- TODO: write every tag file
+
+
+type Tag = String
