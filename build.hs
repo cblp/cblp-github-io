6e0650091af866799e103a652387b668cc9c8431
Yuriy Syrovetskiy <cblp@cblp.su>
2014-10-09 17:23:21 +0400

    [+] reading tags

--- build.hs
+++ build.hs
@@ -1,10 +1,16 @@
 #!/usr/bin/env runhaskell
 {-# OPTIONS -Wall -Werror #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 import Control.Monad (forM)
+import Data.Aeson.TH (defaultOptions, deriveFromJSON, fieldLabelModifier)
+import Data.ByteString as ByteString (readFile)
+import Data.ByteString.Char8 as ByteString (lines, pack, unlines)
 import Data.List (elemIndices)
---import Data.Map (Map)
+import Data.Text (Text)
+import Data.Text as Text (pack)
+import Data.Yaml as Yaml (decode)
 import Development.Shake
 
 
@@ -22,24 +28,34 @@ main = shakeArgs shakeOptions $ do
         -- read tags
         tags_paths :: [(Tag, FilePath)] <- do
             forM postFiles $ \postFile -> do
-                postFileLines <- readFileLines postFile
-                let fmStart:fmEnd:_ =
-                        elemIndices "---" postFileLines
-                    frontmatter =
-                        substr (fmStart + 1) fmEnd postFileLines |> unlines
+                postFileContent <- liftIO $ ByteString.readFile postFile
+                let postFileLines = ByteString.lines postFileContent
+                    fmStart:fmEnd:_ = elemIndices documentStart postFileLines
+                    Just (frontmatter :: Frontmatter) =
+                        substr (fmStart + 1) fmEnd postFileLines
+                        |> ByteString.unlines
+                        |> Yaml.decode
                 liftIO $ print frontmatter
-                return ("tag", postFile)
+                return (Text.pack "tag", postFile)
         liftIO $ print tags_paths
 
         -- TODO: write tag list
         -- TODO: write every tag file
 
   where
+    documentStart = ByteString.pack "---"
     substr start end = drop start . take end
 
 
-type Tag = String
+type Tag = Text
 
 
 (|>) :: a -> (a -> b) -> b
 x |> f = f x
+
+
+data Frontmatter = Frontmatter
+    { fm_tags :: [Tag]
+    }
+        deriving Show
+deriveFromJSON defaultOptions{fieldLabelModifier = drop 3} ''Frontmatter
