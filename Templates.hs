1ef2a8a4000abf90cace8cbb9978e503d6909655
Yuriy Syrovetskiy <cblp@cblp.su>
2015-02-12 18:23:33 +0300

    [+] tempate Content

--- Templates.hs
+++ Templates.hs
@@ -1,26 +1,27 @@
-module Templates    ( Template(..)
-                    , applyTemplate
-                    )
+module Templates  ( Template(..)
+                  , applyTemplate
+                  )
 where
 
-import Hakyll       ( Compiler
-                    , Context
-                    , Identifier
-                    , Item
-                    , fromFilePath
-                    , loadAndApplyTemplate
-                    , relativizeUrls
-                    )
+import Hakyll     ( Compiler
+                  , Context
+                  , Identifier
+                  , Item
+                  , fromFilePath
+                  , loadAndApplyTemplate
+                  , relativizeUrls
+                  )
 
 
 type HakyllTemplater = Context String -> Item String -> Compiler (Item String)
 
 
-data Template   = Archive
-                | Default
-                | Page
-                | PostPage
-                | PostWidget
+data Template = Archive
+              | Content
+              | Default
+              | Page
+              | PostPage
+              | PostWidget
     deriving (Show)
 
 
@@ -28,20 +29,24 @@ templateFile :: Template -> Identifier
 templateFile t = fromFilePath $ "templates/" ++ show t ++ ".html"
 
 
-applyTemplate ::
-    Template -> Context String -> Item String -> Compiler (Item String)
-applyTemplate Default       = \ctx item ->  templateBody Default ctx item
-                                            >>= relativizeUrls
-applyTemplate Page          = inherit Default   Page
-applyTemplate PostPage      = inherit Page      PostPage
-applyTemplate Archive       = inherit Page      Archive
-applyTemplate PostWidget    = templateBody PostWidget
+applyTemplate :: Template -> HakyllTemplater
+applyTemplate Archive     = templateBody Archive  & wrap Content & wrap Page
+applyTemplate Content     = templateBody Content
+applyTemplate Default     = \ctx item ->  templateBody Default ctx item
+                                          >>= relativizeUrls
+applyTemplate Page        = templateBody Page     & wrap Default
+applyTemplate PostPage    = templateBody PostPage & wrap Content & wrap Page
+applyTemplate PostWidget  = templateBody PostWidget
 
 
-inherit ::  Template -> Template -> HakyllTemplater
-inherit parent = \tpl ctx item ->   templateBody tpl ctx item
+wrap :: Template -> HakyllTemplater -> HakyllTemplater
+wrap parent = \tplr ctx item ->  tplr ctx item
                                     >>= applyTemplate parent ctx
 
 
 templateBody :: Template -> HakyllTemplater
 templateBody tpl = loadAndApplyTemplate $ templateFile tpl
+
+
+(&) :: a -> (a -> b) -> b
+x & f = f x
